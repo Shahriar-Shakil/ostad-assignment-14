5cf2874369e78cf06160d029bd4fc027c175a832
shahriar <amivaloasi>
2023-09-06 21:11:52 +0600

    .

--- src/app/components/loginForm.jsx
+++ src/app/components/loginForm.jsx
@@ -30,6 +30,10 @@ export default function LoginForm() {
     <div>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <p>
+            email : email@email.com <br /> pass : 123
+          </p>
+
           <form
             className="space-y-6"
             onSubmit={handleSubmit}
